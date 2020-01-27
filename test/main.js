94be1c7db6ae1071051c4bb941d267312c8991e0
Alejandro <castillo.alex97@outlook.com>
2020-01-27 01:00:08 -0400

    Improved unit tests

--- test/main.js
+++ test/main.js
@@ -1,16 +1,45 @@
 var http = require('http');
+var assert = require('assert');
 var Tundra = require('../src/tundra.js');
-var view = new Tundra({
-    'base': 'views',
-    'extension': 'html',
-    'cache': 'cache'
+
+// Test
+describe('Tundra', function() {
+    let view = new Tundra();
+
+    it('getBase() should return the previously set base value through setBase()', function() {
+        let actual = 'test/views';
+        view.setBase(actual);
+        assert.equal(actual, view.getBase());
+    });
+
+    it('getExtension() should return the previously set extension value through setExtension()', function() {
+        let actual = 'html';
+        view.setExtension(actual);
+        assert.equal(actual, view.getExtension());
+    });
+
+    it('getEncoding() should return the previously set encoding value through setEncoding()', function() {
+        let actual = 'UTF-8';
+        view.setEncoding(actual);
+        assert.equal(actual, view.getEncoding());
+    });
+
+    it('exists() should return true when a view exists (\'home\')', function() {
+        assert.equal(true, view.exists('home'));
+    });
+
 });
 
 http.createServer((req, res) => {
-    var data = {
+    let view = new Tundra({
+        'base': 'test/views',
+        'extension': 'html'
+    });
+
+    let data = {
         title: 'Tundra',
         msg: 'Hello World!',
-        no_escaped_html: '<p>This is a paragraph</p>',
+        no_escaped_html: '<b>This is text between b tags (html characters not escaped).</b>',
         view_exists: view.exists('home')
     };
 
